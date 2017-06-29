f48dac2bc0bc474b684f42898ef6f7dcb0beeeb0
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-06-28 16:46:00 -1000

    insertion sort works

--- insertionsort.js
+++ insertionsort.js
@@ -2,12 +2,19 @@ var unsorted = [5, 10, 2, 15, 7];
 
 function insertionSort(arr){
   for(var i = 0; i < arr.length; i++){
-    for(var j = 0; i < arr.length; j++){
+    var current = arr[i];
+    var prev = i - 1;
 
+    while (prev >= 0 && arr[prev] > current){
+      arr[prev + 1] = arr[prev];
+      prev--;
     }
-    var temp;
-
-
+    arr[prev + 1] = current;
   }
+
+  return arr;
 }
 
+insertionSort(unsorted);
+var sorted = insertionSort(unsorted);
+console.log(sorted);
