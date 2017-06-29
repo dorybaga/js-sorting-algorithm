a5f9f6c11a8ed4c0692c1c4cec69c875df7dc846
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-06-28 16:12:39 -1000

    set up for loop to loop thru array of num

---
+++ insertionsort.js
@@ -0,0 +1,13 @@
+var unsorted = [5, 10, 2, 15, 7];
+
+function insertionSort(arr){
+  for(var i = 0; i < arr.length; i++){
+    for(var j = 0; i < arr.length; j++){
+
+    }
+    var temp;
+
+
+  }
+}
+
